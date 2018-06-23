4c9c7010423c0a58132b6d4520e1f32363b61335
LynnLin-00 <lin.ling0927@hotmail.com>
2018-06-22 23:43:22 -0400

    register

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 
@@ -33,6 +34,7 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
+      isSignedIn: false,
     }
   }
 
@@ -54,7 +56,6 @@ class App extends Component {
   }
 
   displayFaceBox = (box) => {
-    console.log(box);
     this.setState({box: box});
   }
 
@@ -69,34 +70,39 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+      
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route});
-    // if (route === 'signout') {
-    //   this.setState({isSignedIn: false})
-    // } else if (route === 'home') {
-    //   this.setState({isSignedIn: true})
-    // }
-    // this.setState({route: route});
   }
 
   render() {
-    const box = this.state.box;
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className = "App">
         <Particles className = 'particles'
           params={particlesOptions}              
         />
-        <Navigation onRouteChange={this.onRouteChange}/>
-        { this.state.route === 'signin'
-          ? <Signin onRouteChange = {this.onRouteChange}/>
-          : <div>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        { this.state.route === 'home'
+          ? <div>
               <Logo />
               <Rank />
               <ImageLinkForm 
                 onInputChange = {this.onInputChange} 
                 onButtonSubmit = {this.onButtonSubmit}
               />
-              <FaceRecognition box={box} imageUrl={this.state.imageUrl}/>
+              <FaceRecognition box={box} imageUrl={imageUrl}/>
             </div>
+          : (
+               route === 'signin' 
+               ? <Signin onRouteChange = {this.onRouteChange}/>
+               : <Register onRouteChange = {this.onRouteChange}/>
+            )
+         
         }
         
       </div>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-const Signin = ({ onRouteChange }) => {
+const Register = ({ onRouteChange }) => {
   return (  //http://tachyons.io/components/forms/sign-in/index.html
   	//http://tachyons.io/components/cards/product-card/index.html
-  	//do not use <form> because it will automatically send a form, we are going to use json and ajax, which is more dynamic and can be customized better
   	<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-3 center">
 	    <main className="pa4 black-80">
-		  <div className="measure">
+		  <div className="measure">  
 		    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-		      <legend className="f2 fw6 ph0 mh0">Sign In</legend>
+		      <legend className="f2 fw6 ph0 mh0">Register</legend>
+		      <div className="mt3">
+		        <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+		        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
+		      </div>
 		      <div className="mt3">
 		        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
 		        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
@@ -24,17 +27,14 @@ const Signin = ({ onRouteChange }) => {
 		      	onClick = {() => onRouteChange('home')} //defining an arrow function which will be called
 		      	className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
 		      	type="submit" 
-		      	value="Sign in" 
+		      	value="Register" 
 		      />
 		    </div>
-		    <div className="lh-copy mt3">
-		      <p onClick = {() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
-		      
-		    </div>
+		    
 		  </div>
 		</main>
 	</article>
   );
 }
 
-export default Signin;
+export default Register;
